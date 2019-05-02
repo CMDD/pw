3830112b9317418b45a92825a9da00a735ad7227
Desarrollo <desarrollo@iMac-de-John.local>
2019-05-02 11:26:41 -0500

    Login

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Iniciar sesión</title>
+    <link rel="stylesheet" href="panel/css/login.css" type='text/css' media='all'>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <div class="panel left">
+            <div class="content">
+                <h2>Que nadie se quede sin servir</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae sollicitudin mi, ac sodales elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque non erat metus. Mauris varius erat turpis, vel tincidunt justo lacinia a. Donec vitae accumsan tellus. Cras ex mi, imperdiet a dapibus ac, maximus in dui. Praesent eu tempor neque, lobortis porta eros.</p>
+            </div>
+        </div>
+        <div class="panel right">
+            <div class="content">
+                <img src="panel/img/logo-cccmd.svg" class="logo">
+                <h2>Iniciar sesión</h2>
+                <form>
+                    <input type="email" placeholder="E-mail">
+                    <input type="password" placeholder="Contraseña">
+                    <button type="submit" class="btn"><span>Iniciar sesión</span></button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
